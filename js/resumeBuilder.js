aa85d4ba007e4d791322542a2106235e0ce4b77c
Brian <brianpchsu@gmail.com>
2014-10-15 23:39:14 -0400

    Add awesome thougth and fun thought.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,9 @@
+
+
+var awesomethought = "I am Brian and I am awesome!";
+console.log(awesomethought);
+
+var funthought = awesomethought.replace("awesome", "fun");
+console.log(funthought);
+
+$("#main").append(funthought);
